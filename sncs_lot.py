56a2823809393a91c70c5adb43704ecbd71e8a4f
蔡涵宇 <102476562+Henry4234@users.noreply.github.com>
2025-02-18 17:09:08 +0800

    fix run & re-search
    [修改]run : for loop 改為 while，因應查無結果時可以回復查詢
    [新增]查無結果時，回復查詢。

--- sncs_lot.py
+++ sncs_lot.py
@@ -1,5 +1,5 @@
 #autherise by Henry Tsai
-import sys,os, glob,zipfile,re,csv,threading, drivertester
+import sys,os, glob,zipfile,py7zr,re,csv,threading, drivertester
 from dotenv import load_dotenv
 
 from tkinter import filedialog,messagebox
@@ -52,6 +52,7 @@ class DOWNLOAD:
         match step:
             case "輸入新批號":
                 self.input_lotno = input("\n請刷條碼/輸入新批號:")
+                self.current_index+=1   #改變狀態
                 # print(self.input_lotno)
             case "從SNCS爬取所需批號":
                 print("正在從SNCS爬取所需批號...")
@@ -95,42 +96,79 @@ class DOWNLOAD:
                     self.result_title.append(title.text)
                 
                 driver.minimize_window()
+                if len(self.result_title) == 0:
+                    print("根據'%s'查無任何結果!請重新搜索!"%(self.input_lotno))
+                    return "input_not_found"
 
-                print("目前根據'%s'搜索到的結果如下:"%(self.input_lotno))
-                for i in range(1,len(search_h4_result)+1):
-                    print(str(i) + ". " + search_h4_result[i-1].text)
-                self.download_No = input("請選擇要下載的編號:")
-                # sleep(15)
-                driver.get(self.result_link[int(self.download_No) - 1])
-                # 執行下載後
-                self.wait_for_download_complete(self.folder_path)
+                else:
+                    print("目前根據'%s'搜索到的結果如下:"%(self.input_lotno))
+                    for i in range(1,len(search_h4_result)+1):
+                        print(str(i) + ". " + search_h4_result[i-1].text)
+                    self.download_No = input("請選擇要下載的編號:")
+                    # sleep(15)
+                    driver.get(self.result_link[int(self.download_No) - 1])
+                    # 執行下載後
+                    sleep(1)
+                    self.wait_for_download_complete(self.folder_path)
+                    
+                    self.current_index+=1   #改變狀態
+
             
             case "處理解壓縮檔案":
                 print("正在解壓縮檔案...")
                 # 2. 指定要解壓縮到哪個資料夾
                 list_zipfilename =self.result_title[int(self.download_No) - 1].split(".")
                 extract_folder = os.path.join(self.folder_path, list_zipfilename[0])
-
-                #利用壓縮檔名作為資料夾檔名儲存
-                zipfilepath = os.path.join(self.folder_path, self.result_title[int(self.download_No) - 1])
-                with zipfile.ZipFile(zipfilepath, 'r') as zip_ref:
-                    zip_ref.extractall(path=self.folder_path)
+                zipfile_subname = list_zipfilename[1] 
+                if zipfile_subname == "zip":
+                    #利用壓縮檔名作為資料夾檔名儲存
+                    zipfilepath = os.path.join(self.folder_path, self.result_title[int(self.download_No) - 1])
+                    with zipfile.ZipFile(zipfilepath, 'r') as zip_ref:
+                        zip_ref.extractall(path=self.folder_path)
+                elif zipfile_subname == "7z":
+                    #利用壓縮檔名作為資料夾檔名儲存
+                    zipfilepath = os.path.join(self.folder_path, self.result_title[int(self.download_No) - 1])
+                    with py7zr.SevenZipFile(zipfilepath, 'r') as zip_ref:
+                        zip_ref.extractall(path=self.folder_path)
+                else:
+                    return "unzip_error"
                 #刪除zip檔
                 os.remove(zipfilepath)
-            
+                
+                self.current_index+=1   #改變狀態
+
             case "完成":
                 messagebox.showinfo('土城長庚醫院檢驗科', message='下載整理成功!檔案在%s'%(self.folder_path))
+
+                self.current_index+=1   #改變狀態
+                return "done"
+
 
             
     def run(self):
         """控制流程並顯示進度條"""
         if self.folder_path:
+            self.current_index = 0  # 以 steps[0] 代表步驟1，依此類推
             self.download_dir = self.folder_path.replace("/","\\")
             with tqdm(total=len(self.pbarper), desc="處理進度", ncols=100) as pbar:
-                for step in self.pbarper:
-                    self.download_newlot(step)  # 執行每一個步驟
-                    pbar.set_postfix_str(f"正在處理: {step}")  # 顯示當前步驟
+                while self.current_index < len(self.pbarper):
+                    result = self.download_newlot(self.pbarper[self.current_index])  # 執行每一個步驟
+
+                    if result =="input_not_found":
+                        self.current_index = 0
+                        pbar.update(-1)
+                        continue
+                    elif result =="unzip_error":
+                        print("解壓縮失敗，檔案在%s，請自行解壓縮"%(self.folder_path))
+                        break
+                    elif result =="done":
+                        break
+                    pbar.set_postfix_str(f"正在處理: {self.pbarper[self.current_index]}")  # 顯示當前步驟
                     pbar.update(1)  # 更新進度條
+                # for step in self.pbarper:
+                #     self.download_newlot(step)  # 執行每一個步驟
+                #     pbar.set_postfix_str(f"正在處理: {step}")  # 顯示當前步驟
+                #     pbar.update(1)  # 更新進度條
             print("下載成功!")
             return
         else:
